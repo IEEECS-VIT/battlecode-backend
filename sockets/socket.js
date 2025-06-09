2493dc1858c84566e09ff63a50e5d34f1f9dca1f
alisha873 <ali07062005@gmail.com>
2025-06-09 17:43:47 +0530

    feat: add join room and socket disconnection logic to socket.js

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,3 +1,6 @@
+import prisma from '../config/prisma.js';
+import redisClient from '../config/redis.js';
+
 function generateRoomID() {
   const chars =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -12,9 +15,58 @@ function initializeSocket(io) {
   io.on("connection", (socket) => {
     console.log("A user connected:", socket.id);
 
-    socket.on("createRoom", (settings) => {
-      const roomId = generateRoomID();
-      socket.emit("roomCreated", { roomId, settings });
+    socket.on("createRoom", async (settings) => {
+      try {
+        const roomId = generateRoomID();
+        const {playerAId,playerBId = null,timer = 300} = settings;
+
+        const matchData = {
+          id: roomId,
+          playerAId,
+          duration: timer,
+          ...(playerBId ? { playerBId } : {})}
+
+        await prisma.match.create({
+          data: {id: roomId, duration: timer, playerAId: playerAId , playerBId: "12"},});
+
+        await redisClient.hset(`match:${roomId}`, {playerAId,playerBId,timer});
+
+        socket.emit("roomCreated", { roomId, settings });
+        socket.join(roomId);
+
+      } catch (err) {
+        console.error("Error creating room:", err);
+        socket.emit("error", "Failed to create room.");
+      }
+    });
+
+    socket.on("joinRoom", async ({ roomId, playerId }) => {
+      try {
+        const roomData = await redisClient.hgetall(`match:${roomId}`);
+
+        if (!roomData.playerAId) 
+        {
+          return socket.emit("error", "Room not found.");
+        }
+
+        if (!roomData.playerBId) 
+        {
+          await redisClient.hset(`match:${roomId}`, { playerBId: playerId });
+          await prisma.match.update({
+            where: { id: roomId },
+            data: { playerBId: playerId }});
+        }
+
+        socket.join(roomId);
+        console.log(`User ${playerId} joined room ${roomId}`);
+      } catch (err) {
+        console.error("Error joining room:", err);
+        socket.emit("error", "Failed to join room.");
+      }
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User has disconnected:", socket.id);
     });
   });
 }
